25baae0fa6801d174acbc0e9388c5548432f5d4d
James Wong <james.wtc@@gmail.com>
2020-06-06 02:11:35 +0200

    typo

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,7 @@ module.exports = {
     plugins: {
         'postcss-import': {},
         'postcss-preset-env': {},
-        'tailwindcss': {},
-        'autoprefix': {},
-        'cssnano': {}
+        'autoprefixer': {},
+        'cssnano': {},
     }
 };
